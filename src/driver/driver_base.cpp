477bcbb8b64f54f4ad482459c5942cf810e342a1
Manav Bhatia <bhatiamanav@gmail.com>
2014-10-09 17:02:43 -0400

    -- addition of example 2 with adjoint solution.

--- src/driver/driver_base.cpp
+++ src/driver/driver_base.cpp
@@ -2,6 +2,7 @@
 // MAST includes
 #include "driver/driver_base.h"
 #include "base/nonlinear_implicit_assembly.h"
+#include "base/output_assembly_base.h"
 #include "base/physics_discipline_base.h"
 
 // libMesh includes
@@ -10,12 +11,54 @@
 
 
 bool
-MAST::Driver::nonlinear_solution(MAST::NonlinearImplicitAssembly& assembly) {
+MAST::Driver::nonlinear_solution(MAST::PhysicsDisciplineBase&     discipline,
+                                 MAST::SystemInitialization&      system,
+                                 MAST::NonlinearImplicitAssembly& assembly) {
+    
+    assembly.attach_discipline_and_system(discipline, system);
     
     libMesh::NonlinearImplicitSystem&      nonlin_sys   =
     dynamic_cast<libMesh::NonlinearImplicitSystem&>(assembly.system());
         
     nonlin_sys.solve();
+
+    assembly.clear_discipline_and_system();
+    
+    return true;
+}
+
+
+
+
+bool
+MAST::Driver::output_evaluation(MAST::PhysicsDisciplineBase&     discipline,
+                                MAST::SystemInitialization&      system,
+                                MAST::OutputAssemblyBase&        assembly) {
+    
+    assembly.attach_discipline_and_system(discipline, system);
+
+    assembly.system().assemble_qoi();
+    
+    assembly.clear_discipline_and_system();
+    
+    return true;
+}
+
+
+
+bool
+MAST::Driver::adjoint_solution(MAST::PhysicsDisciplineBase&     discipline,
+                               MAST::SystemInitialization&      system,
+                               MAST::NonlinearImplicitAssembly& nonlin_assembly,
+                               MAST::OutputAssemblyBase&        output_assembly) {
+    
+    nonlin_assembly.attach_discipline_and_system(discipline, system);
+    output_assembly.attach_discipline_and_system(discipline, system);
+    
+    output_assembly.system().adjoint_solve();
+    
+    nonlin_assembly.clear_discipline_and_system();
+    output_assembly.clear_discipline_and_system();
     
     return true;
 }
--- src/driver/driver_base.h
+++ src/driver/driver_base.h
@@ -4,11 +4,27 @@
 
 namespace MAST {
     // Forward declerations
+    class PhysicsDisciplineBase;
+    class SystemInitialization;
     class NonlinearImplicitAssembly;
+    class OutputAssemblyBase;
     
+
     namespace Driver {
         
-        bool nonlinear_solution(MAST::NonlinearImplicitAssembly& assembly);
+        bool nonlinear_solution(MAST::PhysicsDisciplineBase&     discipline,
+                                MAST::SystemInitialization&      system,
+                                MAST::NonlinearImplicitAssembly& assembly);
+
+        bool output_evaluation(MAST::PhysicsDisciplineBase&     discipline,
+                               MAST::SystemInitialization&      system,
+                               MAST::OutputAssemblyBase&        assembly);
+
+    
+        bool adjoint_solution(MAST::PhysicsDisciplineBase&     discipline,
+                              MAST::SystemInitialization&      system,
+                              MAST::NonlinearImplicitAssembly& nonlin_assembly,
+                              MAST::OutputAssemblyBase&        output_assembly);
     }
 }
 
