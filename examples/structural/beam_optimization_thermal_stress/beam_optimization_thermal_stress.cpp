f7e2985f1142bcb7bbf4842f9ca4d3a93650c571
Manav Bhatia <bhatiamanav@gmail.com>
2015-11-18 09:09:27 -0600

    Bug-fix for beam optimization with thermal stresses. The stress output object needed to be told about the thermal load on the element. This example needs further improvement since the stress is independent of section thickness in this case.

--- examples/structural/beam_optimization_thermal_stress/beam_optimization_thermal_stress.cpp
+++ examples/structural/beam_optimization_thermal_stress/beam_optimization_thermal_stress.cpp
@@ -242,6 +242,7 @@ _n_stations(0) {
         e_set.insert(*e_it);
         output->set_elements_in_domain(e_set);
         output->set_points_for_evaluation(pts);
+        output->set_volume_loads(_discipline->volume_loads());
         _outputs.push_back(output);
         
         _discipline->add_volume_output((*e_it)->subdomain_id(), *output);
