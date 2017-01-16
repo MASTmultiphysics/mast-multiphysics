e888d1bd672249ac7281ce1c7c7b02fc76c0785e
Manav Bhatia <bhatiamanav@gmail.com>
2017-01-16 16:09:08 -0600

    removing old unnecessary files from previous commit.

---
+++ src/base/complex_mesh_field_function.h
@@ -0,0 +1,138 @@
+/*
+ * MAST: Multidisciplinary-design Adaptation and Sensitivity Toolkit
+ * Copyright (C) 2013-2017  Manav Bhatia
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __mast__complex_mesh_field_function__
+#define __mast__complex_mesh_field_function__
+
+// MAST includes
+#include "base/field_function_base.h"
+
+
+// libMesh includes
+#include "libmesh/numeric_vector.h"
+#include "libmesh/mesh_function.h"
+
+
+
+namespace MAST {
+    
+    // Forward declerations
+    class SystemInitialization;
+    
+    
+    /*!
+     *    This provides a wrapper FieldFunction compatible class that
+     *    interpolates the solution using libMesh's MeshFunction class.
+     */
+    class ComplexMeshFieldFunction:
+    public MAST::FieldFunction<ComplexVectorX> {
+        
+    public:
+        /*!
+         *   constructor
+         */
+        ComplexMeshFieldFunction(MAST::SystemInitialization& sys,
+                                 const std::string& nm);
+        
+        
+        /*!
+         *   destructor
+         */
+        virtual ~ComplexMeshFieldFunction();
+        
+        
+        virtual void operator() (const libMesh::Point& p,
+                                 const Real t,
+                                 ComplexVectorX& v) const;
+        
+        
+        virtual void perturbation (const libMesh::Point& p,
+                                   const Real t,
+                                   ComplexVectorX& v) const;
+        
+        
+        void init(const libMesh::NumericVector<Real>& sol_re,
+                  const libMesh::NumericVector<Real>& sol_im);
+        
+        
+        void init_perturbation(const libMesh::NumericVector<Real>& dsol_re,
+                               const libMesh::NumericVector<Real>& dsol_im);
+        
+        
+        /*!
+         *    @returns a reference to the libMesh mesh function
+         */
+        std::pair<libMesh::MeshFunction&, libMesh::MeshFunction&>
+        get_function() {
+            
+            libmesh_assert(_function_re);
+            
+            return std::pair<libMesh::MeshFunction&, libMesh::MeshFunction&>
+            (*_function_re, *_function_im);
+        }
+        
+        /*!
+         *    @returns a reference to the libMesh mesh function for the
+         *    perturbation in solution
+         */
+        std::pair<libMesh::MeshFunction&, libMesh::MeshFunction&>
+        get_perturbed_function() {
+            
+            libmesh_assert(_perturbed_function_re);
+            
+            return std::pair<libMesh::MeshFunction&, libMesh::MeshFunction&>
+            (*_perturbed_function_re, *_perturbed_function_im);
+        }
+        
+        
+        /*!
+         *   clear the solution and mesh function data structures
+         */
+        void clear();
+        
+    protected:
+        
+        /*!
+         *  current system for which solution is to be interpolated
+         */
+        MAST::SystemInitialization* _system;
+        
+        /*!
+         *   current solution that is going to be interpolated
+         */
+        libMesh::NumericVector<Real>
+        *_sol_re,
+        *_sol_im,
+        *_perturbed_sol_re,
+        *_perturbed_sol_im;
+        
+        /*!
+         *   the MeshFunction object that performs the interpolation
+         */
+        libMesh::MeshFunction
+        *_function_re,
+        *_function_im,
+        *_perturbed_function_re,
+        *_perturbed_function_im;
+        
+    };
+}
+
+#endif // __mast__complex_mesh_field_function__
+
--- src/base/mesh_field_function.cpp
+++ src/base/mesh_field_function.cpp
@@ -1,6 +1,6 @@
 /*
  * MAST: Multidisciplinary-design Adaptation and Sensitivity Toolkit
- * Copyright (C) 2013-2016  Manav Bhatia
+ * Copyright (C) 2013-2017  Manav Bhatia
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,22 +25,21 @@
 #include "libmesh/dof_map.h"
 
 
-template <typename ValType>
-MAST::MeshFieldFunction<ValType>::
-MeshFieldFunction(const std::string& nm):
-MAST::FieldFunction<ValType>(nm),
+MAST::MeshFieldFunction::
+MeshFieldFunction(MAST::SystemInitialization& sys,
+                  const std::string& nm):
+MAST::FieldFunction<RealVectorX>(nm),
 _use_qp_sol(false),
 _qp_sol(),
-_system(nullptr),
+_system(&sys),
 _sol(nullptr),
-_mesh_function(nullptr)
+_function(nullptr)
 { }
 
 
 
 
-template <typename ValType>
-MAST::MeshFieldFunction<ValType>::~MeshFieldFunction() {
+MAST::MeshFieldFunction::~MeshFieldFunction() {
  
     this->clear();
 }
@@ -51,16 +50,14 @@ MAST::MeshFieldFunction<ValType>::~MeshFieldFunction() {
 
 
 
-namespace MAST {
-template <>
 void
-MeshFieldFunction<RealVectorX>::operator() (const libMesh::Point& p,
-                                                  const Real t,
-                                                  RealVectorX& v) const {
+MAST::MeshFieldFunction::operator() (const libMesh::Point& p,
+                                     const Real t,
+                                     RealVectorX& v) const {
     
     // get the pointer to the mesh function from the master
-    const MAST::MeshFieldFunction<RealVectorX>* master =
-    dynamic_cast<const MAST::MeshFieldFunction<RealVectorX>*>(this->master());
+    const MAST::MeshFieldFunction* master =
+    dynamic_cast<const MAST::MeshFieldFunction*>(this);
     
     // if the element has provided a quadrature point solution,
     // then use it
@@ -70,10 +67,43 @@ MeshFieldFunction<RealVectorX>::operator() (const libMesh::Point& p,
     }
     
     // make sure that the object was initialized
-    libmesh_assert(master->_mesh_function);
+    libmesh_assert(master->_function);
     
     DenseRealVector v1;
-    (*master->_mesh_function)(p, t, v1);
+    (*master->_function)(p, t, v1);
+    
+    // make sure that the mesh function was able to find the element
+    // and a solution
+    libmesh_assert(v1.size());
+    
+    // now copy this to the output vector
+    v = RealVectorX::Zero(v1.size());
+    for (unsigned int i=0; i<v1.size(); i++)
+        v(i) = v1(i);
+}
+
+
+void
+MAST::MeshFieldFunction::perturbation(const libMesh::Point& p,
+                                      const Real t,
+                                      RealVectorX& v) const {
+    
+    // get the pointer to the mesh function from the master
+    const MAST::MeshFieldFunction* master =
+    dynamic_cast<const MAST::MeshFieldFunction*>(this);
+    
+    // if the element has provided a quadrature point solution,
+    // then use it
+    if (master->_use_qp_sol) {
+        v = master->_qp_sol;
+        return;
+    }
+    
+    // make sure that the object was initialized
+    libmesh_assert(master->_perturbed_function);
+    
+    DenseRealVector v1;
+    (*master->_perturbed_function)(p, t, v1);
     
     // make sure that the mesh function was able to find the element
     // and a solution
@@ -88,43 +118,36 @@ MeshFieldFunction<RealVectorX>::operator() (const libMesh::Point& p,
 
 
 
-template <>
 void
-MeshFieldFunction<RealVectorX>::derivative (const MAST::DerivativeType d,
-                                                  const MAST::FunctionBase& f,
-                                                  const libMesh::Point& p,
-                                                  const Real t,
-                                                  RealVectorX& v) const {
+MAST::MeshFieldFunction::derivative (const MAST::FunctionBase& f,
+                                     const libMesh::Point& p,
+                                     const Real t,
+                                     RealVectorX& v) const {
     
     
     libmesh_error_msg("To be implemented.");
 }
-}
 
 
 
 
 
-template <typename ValType>
 void
-MAST::MeshFieldFunction<ValType>::
-init_for_system_and_solution (MAST::SystemInitialization& sys,
-                              const libMesh::NumericVector<Real>& sol) {
+MAST::MeshFieldFunction::
+init(const libMesh::NumericVector<Real>& sol,
+     const libMesh::NumericVector<Real>* dsol) {
     
     // this is to be done for the master only
-    const MAST::MeshFieldFunction<ValType>* master_const =
-    dynamic_cast<const MAST::MeshFieldFunction<ValType>*>(this->master());
-    MAST::MeshFieldFunction<ValType>* master =
-    const_cast<MAST::MeshFieldFunction<ValType>*>(master_const);
+    const MAST::MeshFieldFunction* master_const =
+    dynamic_cast<const MAST::MeshFieldFunction*>(this);
+    MAST::MeshFieldFunction* master =
+    const_cast<MAST::MeshFieldFunction*>(master_const);
     
     
     // first make sure that the object is not already initialized
-    libmesh_assert(!master->_mesh_function);
+    libmesh_assert(!master->_function);
     
-    // attach the system
-    master->_system = &sys;
-    
-    libMesh::System& system = sys.system();
+    libMesh::System& system = _system->system();
     
     // next, clone this solution and localize to the sendlist
     master->_sol = libMesh::NumericVector<Real>::build(system.comm()).release();
@@ -141,35 +164,62 @@ init_for_system_and_solution (MAST::SystemInitialization& sys,
     sol.localize(*master->_sol, send_list);
     
     // finally, create the mesh interpolation function
-    master->_mesh_function = new libMesh::MeshFunction(system.get_equation_systems(),
+    master->_function = new libMesh::MeshFunction(system.get_equation_systems(),
                                                        *master->_sol,
                                                        system.get_dof_map(),
-                                                       sys.vars());
-    master->_mesh_function->init();
+                                                       _system->vars());
+    master->_function->init();
+    
+    if (dsol) {
+
+        master->_dsol->init(system.n_dofs(),
+                            system.n_local_dofs(),
+                            send_list,
+                            false,
+                            libMesh::GHOSTED);
+        dsol->localize(*master->_dsol, send_list);
+        
+        // finally, create the mesh interpolation function
+        master->_perturbed_function =
+        new libMesh::MeshFunction(system.get_equation_systems(),
+                                  *master->_dsol,
+                                  system.get_dof_map(),
+                                  _system->vars());
+        master->_perturbed_function->init();
+
+    }
 }
 
 
 
 
-template <typename ValType>
 void
-MAST::MeshFieldFunction<ValType>::clear() {
+MAST::MeshFieldFunction::clear() {
     
     //only the master function will call this on itself
-    if (this->master() != this)
+    if (this != this)
         return;
     
     // if a pointer has been attached, then delete it and the
     // associated vector, and clear the associated system
-    if (_mesh_function) {
-        delete _mesh_function;
-        _mesh_function = nullptr;
+    if (_function) {
+        delete _function;
+        _function = nullptr;
         
         delete _sol;
         _sol = nullptr;
         
         _system = nullptr;
     }
+
+    if (_perturbed_function) {
+        delete _perturbed_function;
+        _perturbed_function = nullptr;
+        
+        delete _dsol;
+        _dsol = nullptr;
+    }
+
     
     // clear flags for quadrature point solution
     _use_qp_sol = false;
@@ -178,16 +228,15 @@ MAST::MeshFieldFunction<ValType>::clear() {
 
 
 
-template <typename ValType>
 void
-MAST::MeshFieldFunction<ValType>::
+MAST::MeshFieldFunction::
 set_element_quadrature_point_solution(RealVectorX& sol) {
     
     // this is to be done for the master only
-    const MAST::MeshFieldFunction<ValType>* master_const =
-    dynamic_cast<const MAST::MeshFieldFunction<ValType>*>(this->master());
-    MAST::MeshFieldFunction<ValType>* master =
-    const_cast<MAST::MeshFieldFunction<ValType>*>(master_const);
+    const MAST::MeshFieldFunction* master_const =
+    dynamic_cast<const MAST::MeshFieldFunction*>(this);
+    MAST::MeshFieldFunction* master =
+    const_cast<MAST::MeshFieldFunction*>(master_const);
 
     master->_use_qp_sol = true;
     master->_qp_sol     = sol;
@@ -195,26 +244,17 @@ set_element_quadrature_point_solution(RealVectorX& sol) {
 
 
 
-template <typename ValType>
 void
-MAST::MeshFieldFunction<ValType>::
+MAST::MeshFieldFunction::
 clear_element_quadrature_point_solution() {
 
     // this is to be done for the master only
-    const MAST::MeshFieldFunction<ValType>* master_const =
-    dynamic_cast<const MAST::MeshFieldFunction<ValType>*>(this->master());
-    MAST::MeshFieldFunction<ValType>* master =
-    const_cast<MAST::MeshFieldFunction<ValType>*>(master_const);
+    const MAST::MeshFieldFunction* master_const =
+    dynamic_cast<const MAST::MeshFieldFunction*>(this);
+    MAST::MeshFieldFunction* master =
+    const_cast<MAST::MeshFieldFunction*>(master_const);
     
     master->_use_qp_sol = false;
     master->_qp_sol.setZero();
-}
-
-
-
-
-// explicit instantiations
-namespace MAST {
-template class MeshFieldFunction<RealVectorX>;
 }
 
