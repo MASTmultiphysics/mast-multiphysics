6172e3fbac18270dceae5b8e1d6462647cae18de
Josh Deaton <jdeaton@users.noreply.github.com>
2018-09-10 17:06:12 -0400

    First dummy example with refactored CMake setup.
    - this is enabled by using more recent version of CMake.

---
+++ ex/ex1_axial_bar_extension/ex1_axial_bar_extension.cpp
@@ -0,0 +1,13 @@
+/*
+* This example solves an axial bar extension problem. It is based on the
+* example of the same name in the MAST source repository
+* (examples/structural/bar_extension).
+*/
+
+// C/C++ includes.
+#include <iostream>
+
+int main(int argc, const char** argv)
+{
+    std::cout << "Hello World!" <<std::endl;
+}
