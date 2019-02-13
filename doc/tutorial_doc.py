2caae9299c8025b1a4fd53c8cbfe441f149405c0
Manav Bhatia <bhatiamanav@gmail.com>
2019-02-12 21:50:45 -0600

    -- Added dome documentation files in doc for Doxygen.

---
+++ doc/tutorial_doc.py
@@ -0,0 +1,85 @@
+import os, sys
+
+
+def sanitize_comment(l):
+    l = l.strip()
+
+    if (len(l) > 1 and l[0:2] ==  "//"):
+        l = " * " + l.strip("//") + " \n"
+
+    return l
+
+def if_line_is_comment(l):
+    ln = l.strip()
+    if ((len(ln) > 0 and ln[0] == "*") or (len(ln) > 1 and ln[0:2] == "//")):
+        return True
+    else:
+        return False
+
+
+def parse_example(d, examples_dir, doc_dir):
+    # the examples are structured such that each directory has one main file named example_XX.cpp,
+    # where, XX is the number of the example.
+    source_abs_path = os.path.join(examples_dir, d, d+".cpp")
+    output_abs_path = os.path.join(     doc_dir, d+".dox")
+    assert(os.path.isfile(source_abs_path))
+
+    # now read the file and translate the documentation
+    example_source_file = open(source_abs_path, "r")
+    example_output_file = open(output_abs_path, "w")
+    translate           = False
+    code_mode           = False
+
+
+    for l in example_source_file:
+        if "BEGIN_TRANSLATE" in l:
+            ln = l.split("BEGIN_TRANSLATE")
+            if len(ln) > 1:
+                example_output_file.write("/*! \n * \page " + d + ln[1] + " \n")
+            else:
+                example_output_file.write("/*! \n * \page " + d + " \n")
+            translate = True
+        elif "END_TRANSLATE" in l:
+            if code_mode:
+                example_output_file.write(" * \endcode \n")
+            example_output_file.write("*/ \n")
+            translate = False
+        elif translate:
+            if if_line_is_comment(l):
+                if code_mode:
+                    example_output_file.write(" * \endcode \n")
+                    code_mode = False
+                example_output_file.write(sanitize_comment(l))
+            else:
+                if not code_mode:
+                    example_output_file.write(" * \code{.cpp} \n")
+                    code_mode = True
+                if (len(l) > 0):
+                    example_output_file.write(l)
+
+    example_source_file.close()
+    example_output_file.close()
+
+
+
+if __name__ == "__main__":
+
+    # the call should be something like $PYTHON tutorial_doc.py $SOURCE_DIR
+    assert(len(sys.argv) == 3)
+
+    examples_dir = sys.argv[1]
+    doc_dir      = sys.argv[2]
+
+    assert(os.path.isdir(examples_dir))
+    assert(os.path.isdir(doc_dir))
+
+    # make sure the subdirectories exists
+    assert(os.path.isdir(examples_dir))
+    assert(os.path.isdir(doc_dir))
+
+    # get the list of example directories to be processed
+    for ex in os.listdir(examples_dir):
+        ex_dir = os.path.join(examples_dir, ex)
+        if ("example_" in ex and os.path.isdir(ex_dir)):
+            parse_example(ex, examples_dir, doc_dir)
+
