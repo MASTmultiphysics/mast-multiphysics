c6f6a99330bb60a060a99ab83d2d0d56e9a73834
Josh Deaton <jdeaton@users.noreply.github.com>
2019-11-15 16:26:00 -0500

    Setup both Debug and Release builds on Travis CI jobs.

--- ci/build_mast.sh
+++ ci/build_mast.sh
@@ -3,13 +3,23 @@
 # Steps common to all OS/toolchains.
 export MAST_INSTALL_DIR=${HOME}/mast || exit
 cd ${TRAVIS_BUILD_DIR} || exit
-mkdir build || exit
-cd build || exit
 
+# Create directories to test both Release (optimized) and Debug build.
+mkdir build_rel || exit
+mkdir build_dbg || exit
+
+# Outer if chooses Linux/macOS based on Travis CI worker.
 if [ "${TRAVIS_OS_NAME}" = linux ]; then # Ubuntu Linux
 
+  # We could test on multiple Ubuntu distributions, which by default supply different versions
+  # of GCC in their default repos. For now we just use 16.04.
   if [ "${TRAVIS_DIST}" = xenial ]; then # Ubuntu 16.04 Xenial Xerus
+
+    # First let us build/install a Release (optimized) version of MAST (-DCMAKE_BUILD_TYPE=Release).
+    echo "TEST RELEASE/OPTIMIZED BUILD..."
+    cd build_rel || exit
     cmake .. \
+      -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="${MAST_INSTALL_DIR}" \
       -DCMAKE_C_COMPILER=mpicc \
       -DCMAKE_CXX_COMPILER=mpic++ \
@@ -24,12 +34,36 @@ if [ "${TRAVIS_OS_NAME}" = linux ]; then # Ubuntu Linux
       -DENABLE_GCMMA=OFF \
       -DENABLE_SNOPT=OFF || exit
 
-    # Build doxygen documentation or build MAST library/examples.
     if [ ${CI_BUILD_DOCS} ]; then
       make doc_doxygen || exit
     else
       make -j 2 || exit
       make install || exit
+    fi
+
+    # Now do a build/install of a Debug version (-DCMAKE_BUILD_TYPE=Debug).
+    echo "TEST DEBUG BUILD..."
+    cd ../build_dbg || exit
+    cmake .. \
+      -DCMAKE_BUILD_TYPE=DEBUG \
+      -DCMAKE_INSTALL_PREFIX="${MAST_INSTALL_DIR}" \
+      -DCMAKE_C_COMPILER=mpicc \
+      -DCMAKE_CXX_COMPILER=mpic++ \
+      -DCMAKE_Fortran_COMPILER=mpifort \
+      -DlibMesh_DIR=/usr/local \
+      -DPETSc_DIR=/usr/lib/petscdir/3.6.2/x86_64-linux-gnu-real \
+      -DSLEPc_DIR=/usr/lib/slepcdir/3.6.1/x86_64-linux-gnu-real \
+      -DEIGEN3_ROOT=/usr/include/eigen3 \
+      -DBOOST_ROOT=/usr \
+      -DBUILD_DOC=ON \
+      -DENABLE_DOT=OFF \
+      -DENABLE_GCMMA=OFF \
+      -DENABLE_SNOPT=OFF || exit
+
+    if [ ${CI_BUILD_DOCS} ]; then
+      echo "No CI documentation for a Debug build."
+    else
+      make -j 2 || exit
     fi
 
   # elif [ "${TRAVIS_DIST}" = bionic ]; then # Ubuntu 18.04 Bionic Beaver
@@ -55,7 +89,11 @@ elif [ "${TRAVIS_OS_NAME}" = osx ]; then # macOS 10.14, XCode 10.2
     libMesh_DIR=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/libmesh-1.5.0-dxjeof7unenkr5ivfpdekiuftgyktkeh
   fi
 
+  # First let us build/install a Release (optimized) version of MAST (-DCMAKE_BUILD_TYPE=Release).
+  echo "TEST RELEASE/OPTIMIZED BUILD..."
+  cd build_rel || exit
   /Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/cmake-3.14.4-vlzpkvowre6hweutogn563ouzkb73jsq/bin/cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${MAST_INSTALL_DIR}" \
     -DCMAKE_C_COMPILER=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openmpi-3.1.4-sep4omvhokkexyojwrahdckfugactovb/bin/mpicc \
     -DCMAKE_CXX_COMPILER=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openmpi-3.1.4-sep4omvhokkexyojwrahdckfugactovb/bin/mpic++ \
@@ -76,6 +114,31 @@ elif [ "${TRAVIS_OS_NAME}" = osx ]; then # macOS 10.14, XCode 10.2
     -DBUILD_DOC=OFF || exit
   make -j 2 || exit
   make install || exit
+
+  # Now do a build/install of a Debug version (-DCMAKE_BUILD_TYPE=Debug).
+  echo "TEST DEBUG BUILD..."
+  cd ../build_dbg || exit
+  /Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/cmake-3.14.4-vlzpkvowre6hweutogn563ouzkb73jsq/bin/cmake .. \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_INSTALL_PREFIX="${MAST_INSTALL_DIR}" \
+    -DCMAKE_C_COMPILER=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openmpi-3.1.4-sep4omvhokkexyojwrahdckfugactovb/bin/mpicc \
+    -DCMAKE_CXX_COMPILER=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openmpi-3.1.4-sep4omvhokkexyojwrahdckfugactovb/bin/mpic++ \
+    -DCMAKE_Fortran_COMPILER=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openmpi-3.1.4-sep4omvhokkexyojwrahdckfugactovb/bin/mpif90 \
+    -DlibMesh_DIR="${libMesh_DIR}" \
+    -DPETSc_DIR="${PETSc_DIR}" \
+    -DSLEPc_DIR="${SLEPc_DIR}" \
+    -DBOOST_ROOT=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/boost-1.70.0-3mel532oks2cxz76mqrxvn4xrl3bg5ri \
+    -DHDF5_ROOT=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/hdf5-1.10.5-kfyeqhtsenqumr7eut6b47mjev7tstom \
+    -DEIGEN3_ROOT=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/eigen-3.3.7-bd2r4aqrkox7dpebj2r3gqvgpqzwuh7x \
+    -DLAPACK_LIBRARIES=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openblas-0.3.7-xqeap7iegoomce3es67cd7exlnq3neue/lib/libopenblas.dylib \
+    -DBLAS_LIBRARIES=/Users/travis/Code/spack-mstc/spack/opt/spack/darwin-mojave-x86_64/clang-10.0.1-apple/openblas-0.3.7-xqeap7iegoomce3es67cd7exlnq3neue/lib/libopenblas.dylib \
+    -DENABLE_GCMMA=OFF \
+    -DENABLE_DOT=OFF \
+    -DENABLE_SNOPT=OFF \
+    -DENABLE_NLOPT=OFF \
+    -DENABLE_CYTHON=OFF \
+    -DBUILD_DOC=OFF || exit
+  make -j 2 || exit
 
 else
   echo "INVALID OS: ${TRAVIS_OS_NAME}"
