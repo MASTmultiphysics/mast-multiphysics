506c9ed81819f6327a8c4f91cc3cb3c6c3a7afaa
Manav Bhatia <bhatiamanav@gmail.com>
2020-01-21 23:57:42 -0600

    adding fix for timpi

--- cmake/FindlibMesh.cmake
+++ cmake/FindlibMesh.cmake
@@ -26,6 +26,18 @@ find_library(timpi_opt_LIBRARY
 find_library(timpi_dbg_LIBRARY
             NAMES timpi_dbg
             HINTS ${libMesh_DIR}/lib)
+            
+if (NOT timpi_opt_LIBRARY)
+        find_library(timpi_opt_LIBRARY
+                    NAMES mesh_opt
+                    HINTS ${libMesh_DIR}/lib)
+endif()
+
+if (NOT timpi_dbg_LIBRARY)
+        find_library(timpi_dbg_LIBRARY
+                    NAMES mesh_dbg
+                    HINTS ${libMesh_DIR}/lib)
+endif()
 
 # If debug library is not available then set it to the optimized library
 if(NOT libMesh_dbg_LIBRARY)
