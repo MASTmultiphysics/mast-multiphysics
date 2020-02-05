34f5fce35f0acc2ff000f4eb22c47bf66e894431
David John Neiferd <13425967+JohnDN90@users.noreply.github.com>
2020-02-05 11:42:52 -0500

    Bug fixes and enhancements to CMake files.
    Fixed bug that could cause timpi libaries to not be found in FindlibMesh.cmake
    Added code to find libmesh_devel (development) libraries
    Added logic to handle cases when when a version (opt, dbg, devel) of libMesh is not present, so that MAST is buildable as long as at least one of these libraries are present

--- cmake/FindlibMesh.cmake
+++ cmake/FindlibMesh.cmake
@@ -3,6 +3,7 @@
 # libMesh_FOUND - system has libMesh
 # libMesh_INCLUDE_DIRS - libMesh include directories.
 # libMesh_dbg_LIBRARIES - libMesh libraries (libmesh_dbg)
+# libMesh_dev_LIBRARIES - libMesh libraries (libmesh_devel)
 # libMesh_opt_LIBRARIES - libMesh libraries (libmesh_opt)
 
 # Find the headers.
@@ -14,42 +15,58 @@ find_library(libMesh_opt_LIBRARY
              NAMES mesh_opt
              HINTS ${libMesh_DIR}/lib)
 
+find_library(timpi_opt_LIBRARY
+             NAMES timpi_opt
+             HINTS ${libMesh_DIR}/lib)
+
+# Find the development libraries.
+find_library(libMesh_dev_LIBRARY
+             NAMES mesh_devel
+             HINTS ${libMesh_DIR}/lib)
+
+find_library(timpi_dev_LIBRARY
+             NAMES timpi_devel
+             HINTS ${libMesh_DIR}/lib)
+
 # Find the debug libraries.
 find_library(libMesh_dbg_LIBRARY
              NAMES mesh_dbg
              HINTS ${libMesh_DIR}/lib)
 
-find_library(timpi_opt_LIBRARY
-            NAMES timpi_opt
-            HINTS ${libMesh_DIR}/lib)
-
 find_library(timpi_dbg_LIBRARY
-            NAMES timpi_dbg
-            HINTS ${libMesh_DIR}/lib)
+             NAMES timpi_dbg
+             HINTS ${libMesh_DIR}/lib)
             
+# If debug library is not available then set it to the optimized library
+if(NOT libMesh_dbg_LIBRARY)
+    if(NOT libMesh_dev_LIBRARY)
+        message(WARNING "Did not find libmesh_dbg or libmesh_devel, using libmesh_opt for debug and devel versions.")
+        find_library(libMesh_dbg_LIBRARY
+                     NAMES mesh_opt
+                     HINTS ${libMesh_DIR}/lib)
+        find_library(libMesh_dev_LIBRARY
+                     NAMES mesh_opt
+                     HINTS ${libMesh_DIR}/lib)
+    else()
+        message(WARNING "Did not find libmesh_dbg using libmesh_devel for debug version.")
+        find_library(libMesh_dbg_LIBRARY
+                     NAMES mesh_devel
+                     HINTS ${libMesh_DIR}/lib)
+    endif()
+endif()
+
+
 if (NOT timpi_opt_LIBRARY)
-        find_library(timpi_opt_LIBRARY
-                    NAMES mesh_opt
-                    HINTS ${libMesh_DIR}/lib)
+    SET(timpi_opt_LIBRARY ${libMesh_opt_LIBRARY})
+endif()
+
+if (NOT timpi_dev_LIBRARY)
+    SET(timpi_dev_LIBRARY ${libMesh_dev_LIBRARY})
 endif()
 
 if (NOT timpi_dbg_LIBRARY)
-        find_library(timpi_dbg_LIBRARY
-                    NAMES mesh_dbg
-                    HINTS ${libMesh_DIR}/lib)
+    SET(timpi_dbg_LIBRARY ${libMesh_dbg_LIBRARY})
 endif()
-
-# If debug library is not available then set it to the optimized library
-if(NOT libMesh_dbg_LIBRARY)
-   message("-- WARN: Did not find libmesh_dbg using libmesh_opt for debug version.")
-    find_library(libMesh_dbg_LIBRARY
-                 NAMES mesh_opt
-                 HINTS ${libMesh_DIR}/lib)
-    find_library(timpi_dbg_LIBRARY
-                 NAMES timpi_opt
-                 HINTS ${libMesh_DIR}/lib)
-endif()
-
 
 # Find libMesh version.
 if(libMesh_INCLUDE_DIR)
@@ -77,22 +94,28 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(libMesh
         REQUIRED_VARS
         libMesh_dbg_LIBRARY
+        libMesh_dev_LIBRARY
         libMesh_opt_LIBRARY
         timpi_dbg_LIBRARY
+        timpi_dev_LIBRARY
         timpi_opt_LIBRARY
         libMesh_INCLUDE_DIR
         VERSION_VAR libMesh_VERSION)
 
 mark_as_advanced(libMesh_INCLUDE_DIR
                  libMesh_dbg_LIBRARY
+                 libMesh_dev_LIBRARY
                  libMesh_opt_LIBRARY
+                 timpi_dbg_LIBRARY
                  timpi_dbg_LIBRARY
                  timpi_opt_LIBRARY
                  libMesh_VERSION
                  libMesh_FOUND)
 
 set(libMesh_dbg_LIBRARIES ${libMesh_dbg_LIBRARY})
+set(libMesh_dev_LIBRARIES ${libMesh_dev_LIBRARY})
 set(libMesh_opt_LIBRARIES ${libMesh_opt_LIBRARY})
 set(timpi_dbg_LIBRARIES ${timpi_dbg_LIBRARY})
+set(timpi_dev_LIBRARIES ${timpi_dev_LIBRARY})
 set(timpi_opt_LIBRARIES ${timpi_opt_LIBRARY})
 set(libMesh_INCLUDE_DIRS ${libMesh_INCLUDE_DIR})
