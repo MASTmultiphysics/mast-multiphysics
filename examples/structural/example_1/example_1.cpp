a80293fc0282200dde2642a27218c03e4b650d6a
Manav Bhatia <bhatiamanav@gmail.com>
2020-05-13 14:03:28 -0500

    -- adding boundary_info.h header to accommodate libMesh refactoring. Minor change in adjoint sensitivity internal routine to remove MPI blocking call for efficiency (#97)

--- examples/structural/example_1/example_1.cpp
+++ examples/structural/example_1/example_1.cpp
@@ -28,6 +28,7 @@
 #include <libmesh/equation_systems.h>
 #include <libmesh/fe_type.h>
 #include <libmesh/numeric_vector.h>
+#include <libmesh/boundary_info.h>
 
 // MAST includes.
 #include "base/nonlinear_system.h"
