e74c956ebfba2d303ac2eed59485ad0fafc4c90d
Josh Deaton <jdeaton@users.noreply.github.com>
2020-05-22 14:36:18 -0400

    Enable temporary website storage/archival for all main repo builds.

--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -10,16 +10,22 @@
 
 cd ${TRAVIS_BUILD_DIR}/website || exit
 
-# Transfer to main hosting if on master branch and not from a Pull Request into master.
-if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "DEPLOY: Deploying master documentation to https://www.mast-multiphysics.com"
+# Transfer to main hosting if on master branch.
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  echo "DEPLOY: Deploying master documentation to https://www.mast-multiphysics.com" || exit
   rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet . ${WEBSITE_HOST_USER}@${WEBSITE_HOST_ADDRESS}:mast-multiphysics.com || exit
 fi
 
 # Transfer temporary version of docs for all builds originating within the primary
-# MASTmultiphysics/mast-multiphysics repository for bush builds and pull request builds.
-# (no forks due to deploy/repo condition in .travis.yml. Storage is identified according to
-# the Travis-CI build number and archives are stored for 24 hours before being removed automatically
+# MASTmultiphysics/mast-multiphysics repository for push builds.
+# (no forks due to deploy/repo condition in .travis.yml, and no "pull request" merged builds since
+# Travis-CI deploy stage does not run for pull requests to prevent archival of arbitrary artifacts)
+# Storage is identified according to the Travis-CI build number and archives are stored for 24 hours before being removed automatically
 # from the web server by an external process.
-CURRENT_TIME="$(date +%Y_%m_%d-%H:%M:%S:%N)"
-echo "${TRAVIS_BUILD_NUMBER}_${CURRENT_TIME}"
+cd .. || exit
+CURRENT_TIME="$(date +%Y_%m_%d-%H_%M_%S_%N)"
+ARCHIVE_NAME="BUILD_NUM_${TRAVIS_BUILD_NUMBER}_TIMESTAMP_${CURRENT_TIME}.tar.gz"
+echo "DEPLOY: Creating website archive: ${ARCHIVE_NAME}" || exit
+tar -zcf "${ARCHIVE_NAME}" website || exit
+echo "DEPLOY: Transferring archive to https://temp-docs.mast-multiphysics.com" || exit
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -r "${ARCHIVE_NAME}" ${WEBSITE_HOST_USER}@${WEBSITE_HOST_ADDRESS}:temp-docs.mast-multiphysics.com || exit
