dedec73daa125fa7c4fa6b974420103cc24e52b7
David John Neiferd <13425967+JohnDN90@users.noreply.github.com>
2020-09-03 16:37:54 -0400

    Updated build_structural_element call in external_force_output.cpp due to updates elsewhere in MAST.

--- src/elasticity/external_force_output.cpp
+++ src/elasticity/external_force_output.cpp
@@ -315,7 +315,7 @@ MAST::ExternalForceOutput::init(const MAST::GeomElem& elem) {
     dynamic_cast<const MAST::ElementPropertyCardBase&>(_discipline->get_property_card(elem));
     
     _physics_elem =
-    MAST::build_structural_element(*_system, *_assembly, elem, p).release();
+    MAST::build_structural_element(*_system, elem, p).release();
 }
 
 
