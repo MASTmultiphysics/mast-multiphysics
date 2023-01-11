aa93c91201233ae728a2c3a2ce07431a1f15cec7
David John Neiferd <627-neiferdj@users.noreply.gitlab.rdte.afrl.dren.mil>
2023-01-11 13:18:55 -0500

    Added volume as an available output. Still need to add effect of displacements to this.
    Added note to both volume and center of mass outputs about the current lack of displacement effects.

--- src/elasticity/volume_output.cpp
+++ src/elasticity/volume_output.cpp
@@ -19,7 +19,7 @@
 
 
 // MAST includes
-#include "elasticity/center_of_mass_output.h"
+#include "elasticity/volume_output.h"
 #include "elasticity/structural_element_base.h"
 #include "base/assembly_base.h"
 #include "base/constant_field_function.h"
@@ -39,61 +39,49 @@
 // libMesh includes
 #include "libmesh/parallel.h"
 
-
-MAST::CenterOfMassOutput::CenterOfMassOutput():
-MAST::OutputAssemblyElemOperations() {
-    _center_of_mass = RealVectorX::Zero(3,1);
-    _dcenter_of_mass_dp = RealVectorX::Zero(3,1);
-    _first_moment_of_inertia_yz = 0.0;
-    _first_moment_of_inertia_xz = 0.0;
-    _first_moment_of_inertia_xy = 0.0;
-    _dfirst_moment_of_inertia_yz_dp = 0.0;
-    _dfirst_moment_of_inertia_xz_dp = 0.0;
-    _dfirst_moment_of_inertia_xy_dp = 0.0;
-    _mass = 0.0;
-    _dmass_dp = 0.0;
-}
-
-
-MAST::CenterOfMassOutput::~CenterOfMassOutput() {
-
-}
-
-
-void
-MAST::CenterOfMassOutput::zero_for_analysis() {
-    _center_of_mass.setZero();
-    _dcenter_of_mass_dp.setZero();
-    _first_moment_of_inertia_yz = 0.0;
-    _first_moment_of_inertia_xz = 0.0;
-    _first_moment_of_inertia_xy = 0.0;
-    _mass = 0.0;
-
-}
-
-
-void
-MAST::CenterOfMassOutput::zero_for_sensitivity() {
+MAST::VolumeOutput::VolumeOutput():
+MAST::OutputAssemblyElemOperations(),
+_volume       (0.),
+_dvolume_dp   (0.)  {
     
-    _center_of_mass.setZero();
-    _dcenter_of_mass_dp.setZero();
+}
+
+
+
+
+MAST::VolumeOutput::~VolumeOutput() {
+
+}
+
+
+
+
+void
+MAST::VolumeOutput::zero_for_analysis() {
+    
+    _volume         = 0.;
+    _dvolume_dp     = 0.;
+}
+
+
+
+void
+MAST::VolumeOutput::zero_for_sensitivity() {
+    
+    _volume         = 0.;
+    _dvolume_dp     = 0.;
 }
 
 
 void
-MAST::CenterOfMassOutput::evaluate() {
+MAST::VolumeOutput::evaluate() {
     
     // make sure that this has not been initialized and calculated for all elems
     libmesh_assert(_physics_elem);
 
     if (this->if_evaluate_for_element(_physics_elem->elem())) {
-
-        // Get the density field function
+        // Get the section property card
         const MAST::ElementPropertyCardBase& section_property = dynamic_cast<const MAST::ElementPropertyCardBase&>(_discipline->get_property_card(_physics_elem->elem()));
-        const MAST::FieldFunction<Real>& rho_f  = section_property.get_material().get<MAST::FieldFunction<Real>>("rho");
-        // Another method of doing this below
-        // MAST::StructuralElementBase& e = dynamic_cast<MAST::StructuralElementBase&>(*_physics_elem);
-        // const MAST::FieldFunction<Real>& rho_f = const_cast<MAST::MaterialPropertyCardBase&>(e.elem_property().get_material()).get<MAST::FieldFunction<Real>>("rho");
 
         // Get the integration points and weights
         // TODO: Currently using the same integration scheme as for element building. Need to allow using user-specified integration scheme
@@ -103,7 +91,6 @@ MAST::CenterOfMassOutput::evaluate() {
         const unsigned int n_qp = (unsigned int)fe->get_qpoints().size();
 
         // Initialize some variables
-        Real rho = 0.0;
         Real Vc = 0.0;
         const MAST::FieldFunction<Real>* Vc_f;
 
@@ -165,45 +152,30 @@ MAST::CenterOfMassOutput::evaluate() {
             Vc_f = &one_f;
         }
 
-        Real mass_elem = 0.0;
         Real time = 0.0;
         // Iterate over the integration points to perform the numerical integration
         for (unsigned int ind_qp=0; ind_qp<n_qp; ind_qp++) {
             // Get the current time of the system
             time = _system->system().time;
-
-            // Get the density at this integration point
-            rho_f(xyz[ind_qp], time, rho);
             // Get the extra volume component at this integration point
             (*Vc_f)(xyz[ind_qp], time, Vc);
-
-            mass_elem = rho * Vc * JxW[ind_qp]; // A value used in multiple calculations below
-
-            _first_moment_of_inertia_yz += mass_elem * xyz[ind_qp](0);
-            _first_moment_of_inertia_xz += mass_elem * xyz[ind_qp](1);
-            _first_moment_of_inertia_xy += mass_elem * xyz[ind_qp](2);
-            _mass += mass_elem;
+            _volume += Vc * JxW[ind_qp];
         }
     }
 
-    // FIXME: Center of mass will change with displacements. Need to account for that in these calculations.
+    // FIXME: Since volume changes with displacements, need to account for displacements in this calculations.
 }
 
 
 void
-MAST::CenterOfMassOutput::evaluate_sensitivity(const MAST::FunctionBase &f) {
+MAST::VolumeOutput::evaluate_sensitivity(const MAST::FunctionBase &f) {
     
     // make sure that this has not been initialized ana calculated for all elems
     libmesh_assert(_physics_elem);
     
         if (this->if_evaluate_for_element(_physics_elem->elem())) {
-
-        // Get the density field function
+        // Get the section property card
         const MAST::ElementPropertyCardBase& section_property = dynamic_cast<const MAST::ElementPropertyCardBase&>(_discipline->get_property_card(_physics_elem->elem()));
-        const MAST::FieldFunction<Real>& rho_f  = section_property.get_material().get<MAST::FieldFunction<Real>>("rho");
-        // Another method of doing this below
-        // MAST::StructuralElementBase& e = dynamic_cast<MAST::StructuralElementBase&>(*_physics_elem);
-        // const MAST::FieldFunction<Real>& rho_f = const_cast<MAST::MaterialPropertyCardBase&>(e.elem_property().get_material()).get<MAST::FieldFunction<Real>>("rho");
 
         // Get the integration points and weights
         // TODO: Currently using the same integration scheme as for element building. Need to allow using user-specified integration scheme
@@ -213,8 +185,6 @@ MAST::CenterOfMassOutput::evaluate_sensitivity(const MAST::FunctionBase &f) {
         const unsigned int n_qp = (unsigned int)fe->get_qpoints().size();
 
         // Initialize some variables
-        Real rho = 0.0;
-        Real drho = 0.0;
         Real Vc = 0.0;
         Real dVc = 0.0;
         const MAST::FieldFunction<Real>* Vc_f;
@@ -282,24 +252,17 @@ MAST::CenterOfMassOutput::evaluate_sensitivity(const MAST::FunctionBase &f) {
         for (unsigned int ind_qp=0; ind_qp<n_qp; ind_qp++) {
             // Get the current time of the system
             time = _system->system().time;
-            // Get the density at this integration point
-            rho_f(xyz[ind_qp], time, rho);
-            rho_f.derivative(f, xyz[ind_qp], time, drho); 
             // Get the extra volume component at this integration point
             (*Vc_f)(xyz[ind_qp], time, Vc);
             Vc_f->derivative(f, xyz[ind_qp], time, dVc);
-            Real dxyz = 0.0;  // FIXME: If the parameter is nodal coordinates (shape sensitivity) then this may be nonzero
-            _dfirst_moment_of_inertia_yz_dp += (drho * xyz[ind_qp](0) * Vc + rho * dxyz * Vc + rho * xyz[ind_qp](0) * dVc) * JxW[ind_qp];
-            _dfirst_moment_of_inertia_xz_dp += (drho * xyz[ind_qp](1) * Vc + rho * dxyz * Vc + rho * xyz[ind_qp](1) * dVc) * JxW[ind_qp];
-            _dfirst_moment_of_inertia_xy_dp += (drho * xyz[ind_qp](2) * Vc + rho * dxyz * Vc + rho * xyz[ind_qp](2) * dVc) * JxW[ind_qp];
-            _dmass_dp += (drho * Vc + rho * dVc) * JxW[ind_qp];
+            _dvolume_dp += dVc * JxW[ind_qp];
         }
     }
 }
 
 
 void
-MAST::CenterOfMassOutput::
+MAST::VolumeOutput::
 evaluate_topology_sensitivity(const MAST::FunctionBase &f) {
     
     // the primal data should have been calculated
@@ -307,41 +270,12 @@ evaluate_topology_sensitivity(const MAST::FunctionBase &f) {
     libmesh_assert(f.is_topology_parameter());
 
     libmesh_error_msg("Must be implemented in derived class; " << __PRETTY_FUNCTION__ << " in " << __FILE__ << " at line number " << __LINE__);
-    
-    // if (this->if_evaluate_for_element(_physics_elem->elem())) {
-        
-    //     std::pair<const MAST::FieldFunction<RealVectorX>*, unsigned int>
-    //     val = this->get_elem_boundary_velocity_data();
-        
-    //     if (val.first) {
-
-    //         MAST::StructuralElementBase& e =
-    //         dynamic_cast<MAST::StructuralElementBase&>(*_physics_elem);
-            
-    //         RealVectorX
-    //         vec   = RealVectorX::Zero(e.sol().size());
-            
-    //         RealMatrixX
-    //         dummy = RealMatrixX::Zero(vec.size(), vec.size());
-            
-    //         e.volume_external_residual_boundary_velocity(f,
-    //                                                      val.second,
-    //                                                      *val.first,
-    //                                                      _discipline->volume_loads(),
-    //                                                      false,
-    //                                                      vec,
-    //                                                      dummy);
-            
-    //         // compute the contribution of this element to center_of_mass
-    //         _dcenter_of_mass_dp -= vec.dot(e.sol());
-    //     }
-    // }
 }
 
 
 
 void
-MAST::CenterOfMassOutput::
+MAST::VolumeOutput::
 evaluate_topology_sensitivity(const MAST::FunctionBase &f,
                               const MAST::FieldFunction<RealVectorX> &vel) {
     
@@ -350,95 +284,30 @@ evaluate_topology_sensitivity(const MAST::FunctionBase &f,
     libmesh_assert(f.is_topology_parameter());
 
     libmesh_error_msg("Must be implemented in derived class; " << __PRETTY_FUNCTION__ << " in " << __FILE__ << " at line number " << __LINE__);
-
-    // const MAST::LevelSetIntersectedElem
-    // &elem = dynamic_cast<const MAST::LevelSetIntersectedElem&>(_physics_elem->elem());
-    
-    // // sensitivity only exists at the boundary. So, we proceed with calculation
-    // // only if this element has an intersection in the interior, or with a side.
-    
-    // if (this->if_evaluate_for_element(elem)    &&
-    //     elem.if_elem_has_level_set_boundary()       &&
-    //     elem.if_subelem_has_side_on_level_set_boundary()) {
-        
-    //     MAST::StructuralElementBase& e =
-    //     dynamic_cast<MAST::StructuralElementBase&>(*_physics_elem);
-        
-    //     RealVectorX
-    //     vec   = RealVectorX::Zero(e.sol().size());
-        
-    //     RealMatrixX
-    //     dummy = RealMatrixX::Zero(vec.size(), vec.size());
-        
-    //     e.volume_external_residual_boundary_velocity(f,
-    //                                                  elem.get_subelem_side_on_level_set_boundary(),
-    //                                                  vel,
-    //                                                  _discipline->volume_loads(),
-    //                                                  false,
-    //                                                  vec,
-    //                                                  dummy);
-        
-    //     // compute the contribution of this element to center_of_mass
-    //     _dcenter_of_mass_dp -= vec.dot(e.sol());
-    // }
 }
 
 
 
-RealVectorX
-MAST::CenterOfMassOutput::output_vector_total() {
+Real
+MAST::VolumeOutput::output_total() {
     
-    RealVectorX val = _center_of_mass;
-    Real first_moment_of_inertia_yz = _first_moment_of_inertia_yz;
-    Real first_moment_of_inertia_xz = _first_moment_of_inertia_xz;
-    Real first_moment_of_inertia_xy = _first_moment_of_inertia_xy;
-    Real mass = _mass;
+    Real val = _volume;
     
-    if (!_skip_comm_sum) {
-        _system->system().comm().sum(first_moment_of_inertia_yz);
-        _system->system().comm().sum(first_moment_of_inertia_xz);
-        _system->system().comm().sum(first_moment_of_inertia_xy);
-        _system->system().comm().sum(mass);
-    }
-
-    val.coeffRef(0,0) = first_moment_of_inertia_yz / mass;
-    val.coeffRef(1,0) = first_moment_of_inertia_xz / mass;
-    val.coeffRef(2,0) = first_moment_of_inertia_xy / mass;
+    if (!_skip_comm_sum)
+        _system->system().comm().sum(val);
     
     return val;
 }
 
 
 
-RealVectorX
-MAST::CenterOfMassOutput::output_vector_sensitivity_total(const MAST::FunctionBase& p) {
-
-    Real first_moment_of_inertia_yz = _first_moment_of_inertia_yz;
-    Real first_moment_of_inertia_xz = _first_moment_of_inertia_xz;
-    Real first_moment_of_inertia_xy = _first_moment_of_inertia_xy;
-    Real mass = _mass;
-
-    RealVectorX val = _dcenter_of_mass_dp;
-    Real dfirst_moment_of_inertia_yz_dp = _dfirst_moment_of_inertia_yz_dp;
-    Real dfirst_moment_of_inertia_xz_dp = _dfirst_moment_of_inertia_xz_dp;
-    Real dfirst_moment_of_inertia_xy_dp = _dfirst_moment_of_inertia_xy_dp;
-    Real dmass_dp = _dmass_dp;
+Real
+MAST::VolumeOutput::output_sensitivity_total(const MAST::FunctionBase& p) {
     
-    if (!_skip_comm_sum) {
-        _system->system().comm().sum(first_moment_of_inertia_yz);
-        _system->system().comm().sum(first_moment_of_inertia_xz);
-        _system->system().comm().sum(first_moment_of_inertia_xy);
-        _system->system().comm().sum(mass);
-
-        _system->system().comm().sum(dfirst_moment_of_inertia_yz_dp);
-        _system->system().comm().sum(dfirst_moment_of_inertia_xz_dp);
-        _system->system().comm().sum(dfirst_moment_of_inertia_xy_dp);
-        _system->system().comm().sum(dmass_dp);
-    }
-
-    val.coeffRef(0,0) = (dfirst_moment_of_inertia_yz_dp - first_moment_of_inertia_yz * dmass_dp / mass) / mass;
-    val.coeffRef(1,0) = (dfirst_moment_of_inertia_xz_dp - first_moment_of_inertia_xz * dmass_dp / mass) / mass;
-    val.coeffRef(2,0) = (dfirst_moment_of_inertia_xy_dp - first_moment_of_inertia_xy * dmass_dp / mass) / mass;
+    Real val = _dvolume_dp;
+    
+    if (!_skip_comm_sum)
+        _system->system().comm().sum(val);
 
     return val;
 }
@@ -446,22 +315,22 @@ MAST::CenterOfMassOutput::output_vector_sensitivity_total(const MAST::FunctionBa
 
 
 void
-MAST::CenterOfMassOutput::output_derivative_for_elem(RealVectorX& dq_dX) {
+MAST::VolumeOutput::output_derivative_for_elem(RealVectorX& dq_dX) {
     
     // make sure that this has not been initialized ana calculated for all elems
     libmesh_assert(_physics_elem);
 
-    libmesh_error_msg("Must be implemented in derived class; " << __PRETTY_FUNCTION__ << " in " << __FILE__ << " at line number " << __LINE__);
+    libmesh_error_msg("TODO: Not yet implemented.; " << __PRETTY_FUNCTION__ << " in " << __FILE__ << " at line number " << __LINE__);
 
     if (this->if_evaluate_for_element(_physics_elem->elem())) {
         dq_dX.setZero();
-        // The center of mass is a function of displacements so I need to figure out the correct equation for this
     }
+    // TODO: Implement partial of volume w.r.t. displacements
 }
 
 
 void
-MAST::CenterOfMassOutput::
+MAST::VolumeOutput::
 set_elem_data(unsigned int dim,
               const libMesh::Elem& ref_elem,
               MAST::GeomElem& elem) const {
@@ -471,7 +340,7 @@ set_elem_data(unsigned int dim,
 
 
 void
-MAST::CenterOfMassOutput::init(const MAST::GeomElem& elem) {
+MAST::VolumeOutput::init(const MAST::GeomElem& elem) {
     
     libmesh_assert(!_physics_elem);
     libmesh_assert(_assembly);
